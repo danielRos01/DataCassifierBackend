69cad2cfcfe2c6b734f4877bfdd6bfd3654f49e6
Daniel Socoza <70946178+danielRos01@users.noreply.github.com>
2020-11-22 22:11:55 +0200

    Update urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.urls import path, include
 from .views import RegisterView
-from .views import LoginView
+from .views import LogoutView,LogoutAllView
 
 
 urlpatterns = [
